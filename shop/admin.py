f2873e5c7ff6a94a2fe3708aed8c737910b5807e
weijingran <3156962923@qq.com>
2025-11-03 21:04:18 +0800

    Add image upload feature for products

--- shop/admin.py
+++ shop/admin.py
@@ -8,16 +8,25 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'stock', 'display_tags') # <-- ADDED 'stock'
+    list_display = ('name', 'category', 'price', 'stock', 'display_image')  # Add 'display_image'
+    list_filter = ('category',)
     list_filter = ('category',)
     search_fields = ('name', 'tags__name')
     filter_horizontal = ('tags',)
     # ADDED fields for the edit form
-    fields = ('name', 'description', 'category', 'price', 'stock', 'tags')
+    fields = ('name', 'description', 'category', 'price', 'stock', 'tags', 'image')  # Add 'image' here
 
     def display_tags(self, obj):
         return ", ".join([tag.name for tag in obj.tags.all()])
     display_tags.short_description = 'Tags'
+
+    def display_image(self, obj):  # Method to display the image in the list view
+        if obj.image:
+            return f'<img src="{obj.image.url}" width="100" height="100" />'  # Display image
+        return "No Image"
+
+    display_image.short_description = 'Product Image'
+    display_image.allow_tags = True  # This allows HTML to be rendered in the list view
 
 @admin.register(Interaction)
 class InteractionAdmin(admin.ModelAdmin):
